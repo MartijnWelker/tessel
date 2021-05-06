533cfdfb5e11362f7a10e36c210f68c565d52e70
Martijn Welker <martijnwelker@hotmail.com>
2021-05-06 23:03:02 +0200

    server

--- server/app/index.js
+++ server/app/index.js
@@ -1,10 +1,36 @@
-const WebSocket = require('ws')
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+const WebSocket = require('ws');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(allowCrossDomain);
+
+var server = http.createServer(app);
 
 console.log('starting server');
 
-const wss = new WebSocket.Server({ port: 80 })
+const wss = new WebSocket.Server({ server });
 
-console.log('server started on port 80');
+app.get('/', function (req, res) {
+	res.writeHead('200');
+	res.end('Alive and running\n');
+});
+
+function allowCrossDomain(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '127.0.0.1');
+	res.header('Access-Control-Allow-Origin', 'localhost');
+	res.header('Access-Control-Allow-Origin', '*.rentman.nl');
+	res.header('Access-Control-Allow-Origin', '*.rentman.de');
+	res.header('Access-Control-Allow-Origin', '*.rentman.eu');
+	res.header('Access-Control-Allow-Origin', '*.onlinerentalsoftware.eu');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+	next();
+}
+
 
 wss.on('connection', ws => {
 	console.log('connection received');
@@ -15,3 +41,7 @@ wss.on('connection', ws => {
 
 	ws.send('ho!')
 })
+
+server.listen(80);
+
+console.log('server started on port 80');
